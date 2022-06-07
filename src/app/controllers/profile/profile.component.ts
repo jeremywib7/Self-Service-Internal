b661d505b4a932a255318fd5049e87a315737218
jeremywib <jeremywib7@gmail.com>
2022-06-08 00:54:02 +0700

    async await

--- src/app/controllers/profile/profile.component.ts
+++ src/app/controllers/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ProfileService} from "../../service/profile.service";
 import {environment} from "../../../environments/environment";
+import {MessageService} from "primeng/api";
+import {firstValueFrom, lastValueFrom} from "rxjs";
+import {UserService} from "../../service/user.service";
 
 @Component({
     selector: 'app-profile',
@@ -12,12 +15,60 @@ export class ProfileComponent implements OnInit {
     apiBaseUrl = environment.apiBaseUrl;
     projectName = environment.project;
 
+    uploadedFiles: File;
+
+    isButtonSaveChangesLoading: boolean = false;
+
     constructor(
-        public profileService: ProfileService
+        public profileService: ProfileService,
+        private userService: UserService,
+        private messageService: MessageService
     ) {
     }
 
     ngOnInit(): void {
+    }
+
+    async updateProfile() {
+
+        if (this.profileService.formProfile.valid) {
+
+            this.isButtonSaveChangesLoading = true;
+
+            console.log("saving profile...");
+
+            const res: any = await lastValueFrom(this.userService.updateUser(this.profileService.formProfile.value)).catch(() => {
+                this.isButtonSaveChangesLoading = false;
+            });
+
+            console.log("uploading image...");
+
+            if (this.uploadedFiles !== undefined) {
+                // upload user image
+                if (this.uploadedFiles) {
+                    await lastValueFrom(this.userService.uploadImageFile(this.uploadedFiles,
+                        this.profileService.formProfile.get('id').value)).then(() => {
+                    }).catch(() => {
+                        this.isButtonSaveChangesLoading = false;
+                    });
+                }
+            }
+
+            this.isButtonSaveChangesLoading = false;
+            console.log(res);
+
+
+        } else {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Failed',
+                detail: 'Please check all required fields', life: 3000
+            });
+        }
+    }
+
+    onSelectedImage(event) {
+        this.uploadedFiles = event.currentFiles[0];
     }
 
 }
